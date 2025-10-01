0dce87375a8021fd4a80dfd2e12af9a077b4db59
clouwud <dakshgajbhiye902@gmail.com>
2025-10-01 15:57:22 +0530

    init

---
+++ .config/yazi/init.lua
@@ -0,0 +1,57 @@
+require("yatline"):setup({
+	show_background = false,
+	--theme = my_theme,
+	-- section_separator = { open = "", close = "" },
+	-- part_separator = { open = "", close = "" },
+	-- inverse_separator = { open = "", close = "" },
+
+	section_separator = { open = "", close = "" },
+	part_separator = { open = "|", close = "|" },
+	inverse_separator = { open = "", close = "" },
+
+	header_line = {
+		left = {
+			section_a = {
+				{ type = "line", custom = false, name = "tabs", params = { "left" } },
+			},
+			section_b = {},
+			section_c = {},
+		},
+		right = {
+			section_a = {
+				{ type = "string", custom = false, name = "date", params = { "%A, %d %B %Y" } },
+			},
+			section_b = {
+				{ type = "string", custom = false, name = "date", params = { "%X" } },
+			},
+			section_c = {},
+		},
+	},
+
+	status_line = {
+		left = {
+			section_a = {
+				{ type = "string", custom = false, name = "tab_mode" },
+			},
+			section_b = {
+				{ type = "string", custom = false, name = "hovered_size" },
+			},
+			section_c = {
+				{ type = "string", custom = false, name = "hovered_path" },
+				{ type = "coloreds", custom = false, name = "count" },
+			},
+		},
+		right = {
+			section_a = {
+				{ type = "string", custom = false, name = "cursor_position" },
+			},
+			section_b = {
+				{ type = "string", custom = false, name = "cursor_percentage" },
+			},
+			section_c = {
+				{ type = "string", custom = false, name = "hovered_file_extension", params = { true } },
+				{ type = "coloreds", custom = false, name = "permissions" },
+			},
+		},
+	},
+})
